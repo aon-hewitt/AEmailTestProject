2747c10a937348db8de2ae723cb33df6da3b6698
John Vogen <john.vogen@Aonhewitt.com>
2020-12-17 15:21:15 -0500

    update

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ platform = Gitana.connect(config).then(function () {
 
 
 
-function sendEmail() {
+function sendEmailOrig() {
     node.subchain(platform).then(function () {
         console.log("node: ", node);
 
@@ -83,7 +83,7 @@ function sendEmail() {
     });
 }
 
-function sendEmailNew() {
+function sendEmail() {
     node.subchain(platform).then(function () {
         console.log("node: ", node);
 
