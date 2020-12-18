3842711a18f43f5b7a1eb18630e3b49bbe83b01a
John Vogen <john.vogen@Aonhewitt.com>
2020-12-18 00:22:19 -0500

    simplify

--- js/alpaca2.js
+++ js/alpaca2.js
@@ -1,19 +1,17 @@
 
-var platform;
-var repository;
-var branch;
-var username;
-var password;
-var config;
+//var platform;
+//var repository;
+//var branch;
+//var username;
+//var password;
+//var config;
 var workflowId = 'amexWorkflowShort';
 var emailProviderId = '6b1b6a8e002d85bb28bd';
+username = 'johnvogen';
+password = 'Sammy51856_';
 
 
 
-//var applicationId = '8bd86d95d89e2980b664'; 
-//var projectId = 'f2c138afb9b12ffec417'; 
-//var repositoryId = 'd3760b30a4b894876003'; 
-//var branchId = '71c33d4740fe00932be3'; 
 
 //original good
 var applicationId = '6d5aa7e34b8be727b8d5';
@@ -21,25 +19,19 @@ var projectId = '06fea8ff21b87b9e8358';
 var repositoryId = 'f2c3571d7a2955e7f8a1';
 var branchId = '7935c19b649b9c399528';
 
+//alternate Bad:
+//var applicationId = '8bd86d95d89e2980b664'; 
+//var projectId = 'f2c138afb9b12ffec417'; 
+//var repositoryId = 'd3760b30a4b894876003'; 
+//var branchId = '71c33d4740fe00932be3'; 
 
-username = 'johnvogen';
-password = 'Sammy51856_';
+
 
 //config = {
 //               "username": username,
 //             "password": password,
 //              "baseURL": "/proxy"
 // }
-
-
-//config = {
-//    "clientKey": "9bfbfd0f-49e2-4909-9d10-37a32b88af8f", 
-//    "clientSecret": "j2hIV3Da5L3moRzo0ykv2xbFIAK/02c5g08nDVoWAkXKQpU/aRK3sStjVd0nEAX7YGmjwoijPZ5/x14qNSnx1amvLgeVSWany6Avpnlyf9Q=",
-//    "username": username,
-//    "password": password,
-//    "baseURL": "https://api.cloudcms.com",
-//    "application": "8bd86d95d89e2980b664 "
-//}
 
 config = {
     "clientKey": "1bd1ddc4-37c7-4c80-b69b-b0d8d226cc34",  // 9bfbfd0f-49e2-4909-9d10-37a32b88af8f
@@ -50,6 +42,18 @@ config = {
     "application": "c8a4dc1dd5644f2934be"
 }
 
+//config = {
+//    "clientKey": "9bfbfd0f-49e2-4909-9d10-37a32b88af8f", 
+//    "clientSecret": "j2hIV3Da5L3moRzo0ykv2xbFIAK/02c5g08nDVoWAkXKQpU/aRK3sStjVd0nEAX7YGmjwoijPZ5/x14qNSnx1amvLgeVSWany6Avpnlyf9Q=",
+//    "username": username,
+//    "password": password,
+//    "baseURL": "https://api.cloudcms.com",
+//    "application": "8bd86d95d89e2980b664 "
+//}
+
+
+
+
 Gitana.connect(config, function (err) {
 
 }).then(function () {
@@ -58,6 +62,7 @@ Gitana.connect(config, function (err) {
         repository = this;
         this.readBranch(branchId).then(function () {
             branch = this;
+            sendEmail();
         });
     });
 });
@@ -74,15 +79,9 @@ function sendEmail() {
     workflowConfig.runtime = {};
     workflowConfig.runtime.applicationId = applicationId;
     workflowConfig.runtime.emailProviderId = emailProviderId;
-    //workflowConfig.runtime.repositoryId = repositoryId;
-    //workflowConfig.runtime.branchId = branchId;
-
     var authInfo = platform.getDriver().authInfo;
-
     platform.readDomain(authInfo.principalDomainId).readPrincipal(authInfo.principalId).then(function () {
         var currentUser = this;
-        console.log("platform: ", platform);
-        console.log("workflowId: ", workflowId);
         this.subchain(platform).createWorkflow(workflowId, workflowConfig).then(function () {
             var data = {
                 "coreNodeId": '',
@@ -93,6 +92,4 @@ function sendEmail() {
             });
         });
     });
-
 }
-
