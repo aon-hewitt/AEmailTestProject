e4caa97f092d8ed1f5e101cbad6ffd2f1ceaf61c
John Vogen <john.vogen@Aonhewitt.com>
2020-12-18 00:44:49 -0500

    bad

--- js/alpaca2.js
+++ js/alpaca2.js
@@ -1,49 +1,25 @@
-
 var workflowId = 'amexWorkflowShort';
 var emailProviderId = '6b1b6a8e002d85bb28bd';
 username = 'johnvogen';
 password = 'Sammy51856_';
 
 //original good
-var applicationId = '6d5aa7e34b8be727b8d5';
-var projectId = '06fea8ff21b87b9e8358';
-var repositoryId = 'f2c3571d7a2955e7f8a1';
-var branchId = '7935c19b649b9c399528';
+//var applicationId = '6d5aa7e34b8be727b8d5';
+//var projectId = '06fea8ff21b87b9e8358';
+//var repositoryId = 'f2c3571d7a2955e7f8a1';
+//var branchId = '7935c19b649b9c399528';
 
 //alternate Bad:
-//var applicationId = '8bd86d95d89e2980b664';
-//var projectId = 'f2c138afb9b12ffec417';
-//var repositoryId = 'd3760b30a4b894876003';
-//var branchId = '71c33d4740fe00932be3';
-
-
+var applicationId = '8bd86d95d89e2980b664';
+var projectId = 'f2c138afb9b12ffec417';
+var repositoryId = 'd3760b30a4b894876003';
+var branchId = '71c33d4740fe00932be3';
 
 config = {
-               "username": username,
-             "password": password,
-              "baseURL": "/proxy"
- }
-
-//config = {
-//    "clientKey": "1bd1ddc4-37c7-4c80-b69b-b0d8d226cc34",  // 9bfbfd0f-49e2-4909-9d10-37a32b88af8f
-//    "clientSecret": "CamxJ6k/aNYbuZVV1uTox0imFpsURRugGjt/AD77DGENmJ+U87Z1eh4KBdKtCcY8/Regd9DH8DYWGJ2mcdSCsK3a+aX1WR2ftnxQQ8yg6ck=",
-//    "username": username,
-//    "password": password,
-//    "baseURL": "https://api.cloudcms.com",
-//    "application": "c8a4dc1dd5644f2934be"
-//}
-
-//config = {
-//    "clientKey": "9bfbfd0f-49e2-4909-9d10-37a32b88af8f",
-//    "clientSecret": "j2hIV3Da5L3moRzo0ykv2xbFIAK/02c5g08nDVoWAkXKQpU/aRK3sStjVd0nEAX7YGmjwoijPZ5/x14qNSnx1amvLgeVSWany6Avpnlyf9Q=",
-//    "username": username,
-//    "password": password,
-//    "baseURL": "https://api.cloudcms.com",
-//    "application": "8bd86d95d89e2980b664 "
-//}
-
-
-
+    "username": username,
+    "password": password,
+    "baseURL": "/proxy"
+}
 
 Gitana.connect(config, function (err) {
 
