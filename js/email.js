cc765c3565250d03cec435937583f2d77d99320a
John Vogen <john.vogen@Aonhewitt.com>
2020-12-25 20:34:38 -0500

    try new email provider

--- js/email.js
+++ js/email.js
@@ -1,5 +1,8 @@
 var workflowId = 'amexWorkflowShort';
-var emailProviderId = '6b1b6a8e002d85bb28bd';
+//var emailProviderId = '6b1b6a8e002d85bb28bd';  //platform provider
+var emailProviderId = '5ab62bd0c77fa211e0d1';  //Test provider
+
+
 username = 'johnvogen';
 password = 'Sammy51856_';
 
@@ -20,6 +23,17 @@ config = {
     "password": password,
     "baseURL": "/proxy"
 }
+
+//config = {
+//    "clientKey": " 9bfbfd0f-49e2-4909-9d10-37a32b88af8f",
+//    "clientSecret": "j2hIV3Da5L3moRzo0ykv2xbFIAK/02c5g08nDVoWAkXKQpU/aRK3sStjVd0nEAX7YGmjwoijPZ5/x14qNSnx1amvLgeVSWany6Avpnlyf9Q=",
+//    "username": username,
+//    "password": password,
+//    "baseURL": "https://api.cloudcms.com",
+//    "application": "8bd86d95d89e2980b664"
+//}
+
+
 
 Gitana.connect(config, function (err) {
 }).then(function () {
