1f80fac2c2cf7127d441eb74eba33425d3f4673f
John Vogen <john.vogen@Aonhewitt.com>
2020-12-25 21:06:07 -0500

    ok use the new app id as well

--- js/email.js
+++ js/email.js
@@ -7,13 +7,13 @@ username = 'johnvogen';
 password = 'Sammy51856_';
 
 //original good project - 'American Express' values
-var applicationId = '6d5aa7e34b8be727b8d5';
+//var applicationId = '6d5aa7e34b8be727b8d5';
 //var projectId = '06fea8ff21b87b9e8358';
 //var repositoryId = 'f2c3571d7a2955e7f8a1';
 //var branchId = '7935c19b649b9c399528';
 
 //alternate bad project - 'AEmailTest' values:
-//var applicationId = '8bd86d95d89e2980b664';
+var applicationId = '8bd86d95d89e2980b664';
 var projectId = 'f2c138afb9b12ffec417';
 var repositoryId = 'd3760b30a4b894876003';
 var branchId = '71c33d4740fe00932be3';
