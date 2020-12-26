ca38219e0f02a3050703c7b5446830c3fa3e6409
John Vogen <john.vogen@Aonhewitt.com>
2020-12-25 19:41:43 -0500

    update

--- js/email.js
+++ js/email.js
@@ -3,17 +3,17 @@ var emailProviderId = '6b1b6a8e002d85bb28bd';
 username = 'johnvogen';
 password = 'Sammy51856_';
 
-//original good
-//var applicationId = '6d5aa7e34b8be727b8d5';
-//var projectId = '06fea8ff21b87b9e8358';
-//var repositoryId = 'f2c3571d7a2955e7f8a1';
-//var branchId = '7935c19b649b9c399528';
+//original good project - 'American Express' values
+var applicationId = '6d5aa7e34b8be727b8d5';
+var projectId = '06fea8ff21b87b9e8358';
+var repositoryId = 'f2c3571d7a2955e7f8a1';
+var branchId = '7935c19b649b9c399528';
 
-//alternate Bad:
-var applicationId = '8bd86d95d89e2980b664';
-var projectId = 'f2c138afb9b12ffec417';
-var repositoryId = 'd3760b30a4b894876003';
-var branchId = '71c33d4740fe00932be3';
+//alternate bad project - 'AEmailTest' values:
+//var applicationId = '8bd86d95d89e2980b664';
+//var projectId = 'f2c138afb9b12ffec417';
+//var repositoryId = 'd3760b30a4b894876003';
+//var branchId = '71c33d4740fe00932be3';
 
 config = {
     "username": username,
@@ -22,7 +22,6 @@ config = {
 }
 
 Gitana.connect(config, function (err) {
-
 }).then(function () {
     platform = this;
     this.readRepository(repositoryId).then(function () {
